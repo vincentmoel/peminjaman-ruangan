6dd16ea6a9c2c75c44235c64cb765d661d4ff5fe
Vincent Nathaniel M <vincentmoeljopranoto@gmail.com>
2021-12-28 00:28:36 +0700

    Laravel Sanctum Setup

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Whoops\Handler\PlainTextHandler;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $user = User::where('username',$request->username)->first();
+
+        if(!$user || !Hash::check($request->password,$user->password))
+        {
+            return response()->json([
+                'message' => "Unauthorized"
+            ],401 );
+        }
+
+        $token = $user->createToken('token-name')->plainTextToken;
+
+        return response()->json([
+            "message"   => "success",
+            "user"      => $user,
+            "token"     => $token
+        ],200);
+    }
+}
